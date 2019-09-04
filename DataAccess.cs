1361e63c8d3cf945646a8db6480f433b8660dc15
mai ha an <maihaan24@gmail.com>
2019-09-04 14:27:57 +0700

    XLA cho K61
    XLA cho K61

---
+++ DataAccess.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Map2SQL
+{
+    public class DataAccess
+    {
+        // Thuoc tinh
+        String Connection;
+
+        // Phuong thuc
+        public DataAccess()
+        {
+            Connection = "Data Source=" + Properties.Settings.Default.server
+                + "; Initial catalog=" + Properties.Settings.Default.database
+                + "; User id=" + Properties.Settings.Default.userid
+                + "; Password=" + Properties.Settings.Default.password + ";";
+        }
+
+        public DataTable Read(String query)
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable tb = new DataTable();
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                tb.Load(dr, LoadOption.OverwriteChanges);
+                con.Close();
+                return tb;
+            }
+            catch
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                return null;
+            }
+        }
+
+        public int Write(String query)
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                int dem = 0;
+                dem = cmd.ExecuteNonQuery();
+                con.Close();
+                return dem;
+            }
+            catch
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                return -1;
+            }
+        }
+
+        public int GetMaxColumnValue(String tableName, String columnName)
+        {
+            int ma = 0;
+            String queryMa = "SELECT Max(" + columnName + ") FROM " + tableName;
+            DataTable tb = Read(queryMa);
+            if (tb != null && tb.Rows.Count == 1)
+            {
+                ma = int.Parse(tb.Rows[0][0].ToString());
+            }
+            return ma;
+        }
+    }
+}
---
+++ MyShapefile.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GeoAPI;
+using System.Data;
+
+namespace Map2SQL
+{
+    public class MyShapefile
+    {
+        DataAccess da = new DataAccess();
+        // Thuoc tinh
+        public String FileName { get; set; }
+        public String SQLTableName { get; set; }
+
+        // Phuong thuc
+        public int UpdateToDB()
+        {
+            try
+            {
+                GeometryServiceProvider.Instance = new NetTopologySuite.NtsGeometryServices();
+
+                // Mo tep tin
+                SharpMap.Data.Providers.ShapeFile map = new SharpMap.Data.Providers.ShapeFile(FileName);
+                map.Open();
+
+                String dsCot = "";
+                int demKQ = 0;
+
+                // Doc cau truc cua tep tin
+                var feature = map.GetFeature(0);
+                String query = "CREATE TABLE " + SQLTableName + "(ID int Identity(1,1) PRIMARY KEY, ";
+                foreach(DataColumn col in feature.Table.Columns)
+                {
+                    if (col.ColumnName.ToLower().Equals("id") || col.ColumnName.ToLower().Equals("oid"))
+                        continue;
+                    dsCot += col.ColumnName + ", ";
+
+                    String type = col.DataType.ToString().ToLower().Split('.')[1];
+                    if (type.Equals("string"))
+                        query += col.ColumnName + " nvarchar(max), ";
+                    else if (type.Equals("int16") || type.Equals("int32") || type.Equals("int64")
+                        || type.Equals("uint16") || type.Equals("uint32") || type.Equals("uint64"))
+                        query += col.ColumnName + " int, ";
+                    else if (type.Equals("float") || type.Equals("double"))
+                        query += col.ColumnName + " float, ";
+                }
+                dsCot += "geom";
+                query += "geom Geometry)";
+                da.Write(query);
+
+                // Doc du lieu tren ban do va cap nhat vao CSDL
+                // INSERT INTO tenBang(ds cot) VALUES(ds gia tri)
+                for(int i = 0; i < map.GetFeatureCount(); i++)
+                {
+                    var featurei = map.GetFeature((uint)i);
+                    String insertQuery = "INSERT INTO " + SQLTableName + "(" + dsCot
+                        + ") VALUES(";
+                    String values = "";
+                    int demCot = 0;
+                    foreach(DataColumn col in featurei.Table.Columns)
+                    {
+                        if (col.ColumnName.ToLower().Equals("oid") || col.ColumnName.ToLower().Equals("id"))
+                            continue;
+                        String type = col.DataType.ToString().ToLower().Split('.')[1];
+                        if(type.ToLower().Equals("string"))
+                        {
+                            values += "N'" + featurei.ItemArray[demCot].ToString() + "', ";
+                        }
+                        else
+                        {
+                            values += featurei.ItemArray[demCot].ToString() + ", ";
+                        }
+                    }
+                    String polygon = featurei.Geometry.ToString();
+                    values += "Geometry::STPolyFromText('" + polygon + "', 4326)";
+                    insertQuery += values + ")";
+                    demKQ += da.Write(insertQuery);                    
+                }
+                return demKQ;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
