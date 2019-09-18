bfe5444ea930da0006cc6489f6aff454dd40e7a1
mai ha an <maihaan24@gmail.com>
2019-09-18 13:24:24 +0700

    Update 18092019

---
+++ BaiTap2/Objects/DataAccess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyNhaO.Objects
+{
+    public class DataAccess
+    {
+        String connection = System.Configuration.ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
+        public DataTable Read(String query)
+        {
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable tb = new DataTable();
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                tb.Load(dr, LoadOption.OverwriteChanges);
+                con.Close();
+                return tb;
+            }
+            catch
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                return null;
+            }
+        }
+
+        public int Write(String query)
+        {
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                int dem = 0;
+                dem = cmd.ExecuteNonQuery();
+                con.Close();
+                return dem;
+            }
+            catch
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                return -1;
+            }
+        }
+    }
+}
